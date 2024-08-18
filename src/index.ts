7ccdd87ed82748447e9d6dbe26bad274277c4457
fonchu <hintekk@fonchu.local>
2024-08-19 00:53:24 +0100

    updates 2

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as os from "os";
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send(`Hi! ECS + ECR + FARGATE!`)
+  response.send(`Hello world!  ECS + ECR + FARGATE`)
 })
 
 const PORT = 80
